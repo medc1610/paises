2cab210f75a965f8a3c00a8c02e7424698f9b97f
medc1610 <medc1610@gmail.com>
2021-12-08 00:48:14 -0300

    first commit

---
+++ src/app/paises/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,49 @@
+import { Component} from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  capitales: Country[] = [];
+
+  constructor(private PaisService: PaisService) { }
+
+  buscar(termino: string){
+    
+    this.hayError = false;
+    this.termino = termino;
+   
+    
+
+    this.PaisService.buscarCapital(this.termino)
+        .subscribe( (paises:Country[]) => {
+          console.log(paises);
+          this.capitales = paises;
+          
+        }, (err) => {
+          this.hayError = true;
+          this.capitales = [];
+          
+          
+          
+          
+          
+        });
+
+}
+
+
+  sugerencias(termino:string){
+    this.hayError = false;
+  }
+
+ 
+}
